736dbe6906f6b5f290d52af85849c41c1cb4f6b8
Nozominakamoto <nozomi.nakamoto.likeit@gmail.com>
2018-11-14 16:55:11 +0900

    Ptna16_02まで完了

--- Practice/src/practice15/ptra15/PTra15_02.java
+++ Practice/src/practice15/ptra15/PTra15_02.java
@@ -7,17 +7,16 @@
 package practice15.ptra15;
 
 import practice15.common.Java2Month;
-import practice15.common.Room;
 
 public class PTra15_02 {
 	public static void main(String[] args) {
 
-		Room room = new Java2Month();
+		Java2Month room = new Java2Month();
 
 		// roomに格納されたJava2MonthインスタンスのgetCurriculumメソッドを呼び出してください
-
+		 room.getCurriculum();
 
 		// 取得したカリキュラム情報を全て出力してください
-
+		System.out.println(room.getCurriculum());
 	}
 }

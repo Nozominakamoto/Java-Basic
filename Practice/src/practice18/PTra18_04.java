7a519a1cbeca6f03eea025884cfa8eef1bfc99ea
Nozominakamoto <nozomi.nakamoto.likeit@gmail.com>
2018-11-16 10:45:42 +0900

    Ptra18_04まで完了

--- Practice/src/practice18/PTra18_04.java
+++ Practice/src/practice18/PTra18_04.java
@@ -6,22 +6,63 @@
  */
 package practice18;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class PTra18_04 {
 
 	/*
-	 * PTra19_02で作成したPlayerクラスを使用します
+	 * PTra18_02で作成したPlayerクラスを使用します
 	 */
-
-	public static void main(String[] args) {
-
 		/*
 		 * ①
 		 * ★ file/BestElevenCandidate.csvの内容を取得し、１行毎にPlayerインスタンスに情報を格納してください
 		 * ★ ArrayListを作成して、Playerインスタンスを格納してください
 		 */
+	// ★ ①のArrayListの中からGK1名、DF4名、MF4名, FW2名をランダムで出力してください
+	public static void main(String[] args) {
+
+		ArrayList<Player> array = new ArrayList<>();
+		try (Scanner scanner = new Scanner(new File("file/BestElevenCandidate.csv"))) {
+			while (scanner.hasNext()) {
+				String line = scanner.nextLine();
+				Player player = new Player();
+				String[] list = line.split(",");
 
 
-		// ★ ①のArrayListの中からGK1名、DF4名、MF4名, FW2名をランダムで出力してください
 
-	}
+				player.setPosition(list[0]);
+				player.setName(list[1]);
+				player.setCountry(list[2]);
+				player.setTeam(list[3]);
+
+				array.add(player);
+
+
+
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("ファイルが見つかりません");
+		}
+
+
+		for(int i = 0; i<1;i++) {
+		System.out.println(array.get(0));
+		}
+		for(int j = 5; j<3;j++) {
+		System.out.println(array.get(3));
+		}
+		for(int k = 25; k<3;k++) {
+			System.out.println(array.get(3));
+		}
+		for(int l = 40; l<1;l++) {
+			System.out.println(array.get(1));
+		}
+
+		}
+
+
+
 }
